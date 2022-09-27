3108dd89d05d573112578dbfc54f8e26d39e7316
thavasurya12 <109416662+thavasurya12@users.noreply.github.com>
2022-09-27 22:52:55 +0530

    Update haha.py

--- haha.py
+++ haha.py
@@ -3,8 +3,8 @@
 num1 = 2.6
 num2 = 7.3
 
-# Add two numbers
-sum = num1 + num2
+# sub two numbers
+sum = num1 - num2
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
